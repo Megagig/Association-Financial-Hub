57d4385004c6010637120cb7d3a61702f63754bb
megagig <megagigsoftwaresolutions@gmail.com>
2025-04-23 11:23:56 +0100

    Finishing Touches for frontend registration

--- frontend/src/components/MainLayout/Hero.tsx
+++
@@ -1,9 +0,0 @@
-const Hero = () => {
-  return (
-    <div>
-      <h1>MainLayout Hero Section</h1>
-    </div>
-  );
-};
-
-export default Hero;
--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,6 +1,5 @@
 import Header from '@/components/MainLayout/Header';
 import { Footer } from '@/components/MainLayout/Footer';
-// import Hero from '../components/Hero';
 import { Outlet } from 'react-router-dom';
 
 const MainLayout: React.FC = () => {
